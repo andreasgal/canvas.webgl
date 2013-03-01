fd861820dbc5de7eafda684d7376597224cf136d
Michael Bebenita <mbebenita@mozilla.com>
2013-03-01 01:23:37 -0800

    Playing around with the glu tesselator.

--- src/viewer/viewer.js
+++ src/viewer/viewer.js
@@ -36,6 +36,14 @@ function randomColor() {
   return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 }
 
+function randomGrayColor() {
+  var color = Math.random() * 0xFF;
+  return '#' + (color << 16 | color << 8 | color).toString(16);
+}
+
+var LIGHT_GRAY = "#DDDDDD";
+var DARK_GRAY = "#222222";
+
 function random(max) {
   return (Math.random() * max) | 0;
 }
@@ -51,7 +59,12 @@ function randomPoint(padding) {
   return {x: padding + random(canvas.width - padding * 2), y: padding + random(canvas.height - padding * 2)};
 }
 
+function randomLine(padding) {
+  return {a: randomPoint(padding), b: randomPoint(padding)};
+}
+
 canvasContext.lineWidth = 1;
+canvasContext.lineJoin = 'bevel';
 
 function checkSegmentIntersection(a, b, c, d) {
   var s1_x = b.x - a.x;
@@ -242,19 +255,50 @@ canvas.onmousemove = function (m) {
   var points = [new Vector(100, 100)];
   points.push(new Vector(m.layerX, m.layerY));
   points.push(new Vector(100, 200));
+  points.push(new Vector(200, 200));
+  points.push(new Vector(300, 200));
+  points.push(new Vector(300, 300));
   points.push(new Vector(400, 100));
   ctx.moveTo(points[0].x, points[0].y);
   for (var i = 1; i < points.length; i++) {
     ctx.lineTo(points[i].x, points[i].y);
   }
+  canvasContext.strokeStyle = "#000000";
   ctx.stroke();
 
-  stroke(move(points, 0, 300), 30);
+  stroke(move(points, new Vector(0, 300)), 30);
+
+
+  var radius = m.layerY;
+  ctx.lineWidth = 30;
+  ctx.beginPath();
+  ctx.arc(100, 100, radius, 0, Math.PI * 2, true);
+  ctx.stroke();
+
+
+  var outer = circle({x: 100, y: 300}, radius + 30 / 2, radius);
+  var inner = circle({x: 100, y: 300}, radius - 30 / 2, radius);
+
+  var loops = [vertexBuffer(outer), vertexBuffer(inner.reverse())];
+  drawTesselation(loops);
+  // drawLines(seg, true);
 };
 
-function move(polyline, x, y) {
+function circle(center, radius, count) {
+  var slice = 2 * Math.PI / count;
+  var points = [];
+  for (var i = 0; i < count; i++) {
+    points.push({
+      x: center.x + radius * Math.cos(slice * i),
+      y: center.y + radius * Math.sin(slice * i),
+    });
+  }
+  return points;
+}
+
+function move(polyline, o) {
   return polyline.map(function (v) {
-    return v.add(new Vector(x, y));
+    return v.add(o);
   });
 }
 
@@ -271,16 +315,139 @@ function strokeSegment(a, b, width) {
   ];
 }
 
+function vertexBuffer(vertices) {
+  var buffer = new Float32Array(vertices.length * 2);
+  for (var i = 0; i < vertices.length; i++) {
+    buffer[i * 2] = vertices[i].x;
+    buffer[i * 2 + 1] = vertices[i].y;
+  }
+  return buffer;
+}
+
+function scale(a, x) {
+  return a.map(function (y) {
+    return y * x;
+  });
+}
+
 function stroke(p, width) {
+  var loops = [];
+  for (var i = 0; i < p.length - 1; i++) {
+    var stroke = strokeSegment(p[i], p[i + 1], width);
+    stroke.push(stroke[0]);
+    loops.push(vertexBuffer(stroke));
+  }
+
+  // console.log(tessellate([[0,0,3,0,3,3,0,3],[1,1,2,1,2,2,1,2]]));
+
+  drawTesselation(loops);
+  // drawTesselation([[0,0,300,0,300,300,0,300],[100,100,200,100,200,200,100,200]]);
+
+  /*
+  drawTesselation([
+    [0,0,300,0,300,300,0,300],
+    [100,100,400,0,400,400,0,400]
+  ]);
+  */
+
+
+  // drawTesselation([scale([0,0,3,0,3,3,0,3], 100), scale([1,1,1,2,2,2,2,1], 100)]);
+
+  /*
+  drawTesselation([[0,0,1,5,2,0,-1,3,3,3].map(function (x) {
+    return 100 + x * 100;
+  })]);
+  */
+}
+
+function drawTesselation(loops) {
+  var tess = tessellate(loops);
+  var vertices = tess.vertices;
+  var triangles = tess.triangles;
+  canvasContext.lineWidth = 1;
+  canvasContext.strokeStyle = "rgba(200, 200, 200, 0.5)";
+
+  for (var i = 0; i < triangles.length; i += 3) {
+    var v0 = triangles[i] * 2;
+    var v1 = triangles[i + 1] * 2;
+    var v2 = triangles[i + 2] * 2;
+    canvasContext.beginPath();
+    canvasContext.moveTo(vertices[v0], vertices[v0 + 1]);
+    canvasContext.lineTo(vertices[v1], vertices[v1 + 1]);
+    canvasContext.lineTo(vertices[v2], vertices[v2 + 1]);
+    canvasContext.lineTo(vertices[v0], vertices[v0 + 1]);
+    canvasContext.fillStyle = DARK_GRAY;
+    canvasContext.fill();
+    canvasContext.stroke();
+  }
+
+}
+
+function stroke2(p, width) {
   // var segment = strokeSegment(p[0], p[1], width);
   // drawLines(strokeSegment(p[0], p[1], width), true);
 
+  var accumulator;
   for (var i = 0; i < p.length - 1; i++) {
-    drawLines(strokeSegment(p[i], p[i + 1], width), true);
+    var stroke = strokeSegment(p[i], p[i + 1], width);
+    var polygon = [{isHole: false, vertices: stroke}];
+    if (accumulator) {
+      accumulator = gpc.clip(accumulator, 3, polygon);
+    } else {
+      accumulator = polygon;
+    }
   }
 
+  for (var i = 0; i < accumulator.length; i++) {
+    drawLines(accumulator[i].vertices, true);
+  }
+
+
+
+  // vertices.push(vertices[0]);
+
+  // canvasContext.strokeStyle = "#000000";
+  canvasContext.strokeStyle = "rgba(255, 0, 0, 0.5)";
+
+  for (var c = 0; c < accumulator.length; c++) {
+    var vertices = accumulator[c].vertices;
+    var loop = [];
+    for (var i = 0; i < vertices.length; i++) {
+      loop.push(vertices[i].x);
+      loop.push(vertices[i].y);
+    }
+
+    var tess = tessellate([loop]);
+    // console.info(tess);
+    var vertices = tess.vertices;
+    var triangles = tess.triangles;
+    canvasContext.beginPath();
+    for (var i = 0; i < triangles.length; i += 3) {
+      var v0 = triangles[i] * 2;
+      var v1 = triangles[i + 1] * 2;
+      var v2 = triangles[i + 2] * 2;
+      canvasContext.moveTo(vertices[v0], vertices[v0 + 1]);
+      canvasContext.lineTo(vertices[v1], vertices[v1 + 1]);
+      canvasContext.lineTo(vertices[v2], vertices[v2 + 1]);
+    }
+    canvasContext.stroke();
+  }
+
+  /*
   var polygon = [];
-  for (var i = 0; i < p.length - 2; i++) {
+  for (var i = 0; i < p.length - 1; i++) {
+    var stroke = strokeSegment(p[i], p[i + 1], width);
+
+    drawLines(strokeSegment(p[i], p[i + 1], width), true);
+  }
+  */
+
+
+  /*
+  var polygon = [];
+
+  // for (var i = 0; i < p.length - 2; i++) {
+  for (var i = 0; i < 1; i++) {
     var a = p[i];
     var b = p[i + 1];
     var c = p[i + 2];
@@ -288,13 +455,18 @@ function stroke(p, width) {
     var ab = strokeSegment(a, b, width);
     var bc = strokeSegment(b, c, width);
 
-    drawLines(move(ab, 0, 200), true);
-    drawLines(move(bc, 0, 200), true);
+    polygon.push(ab[0]);
+
+    // console.info(Vector.signedArea(a, b, c));
+
+    // drawLines(move(ab, 0, 200), true);
+    // drawLines(move(bc, 0, 200), true);
     // drawLines(strokeSegment(p[i], p[i + 1], width), true);
   }
+
+  drawLines(polygon, true, new Vector(0, 200));
+  */
 }
-
-
 
 function drawLines(p, close, offset) {
   var ctx = canvasContext;
@@ -341,5 +513,74 @@ var Vector = (function () {
     var length = this.getLength();
     return new Vector(this.x / length, this.y / length);
   };
+  /**
+   * < 0 if ab, bc form a left turn, 0 if colinear, or > 0 otherwise.
+   */
+  constructor.signedArea = function (a, b, c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+  };
   return constructor;
 })();
+
+function drawSegments() {
+  var count = 5;
+  var lines = [];
+  for (var i = 0; i < count; i++) {
+    lines.push(randomLine());
+  }
+  var ctx = canvasContext;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.forEach(function (l) {
+    ctx.beginPath();
+    ctx.moveTo(l.a.x, l.a.y);
+    ctx.lineTo(l.b.x, l.b.y);
+    ctx.stroke();
+  });
+
+  findIntersections(lines);
+}
+
+function flipLinePoints(l) {
+  if (comparePoints(l.a, l.b) > 0) {
+    var t = l.a;
+    l.a = l.b;
+    l.b = t;
+  }
+}
+
+function comparePoints(a, b) {
+  var ax = a.x;
+  var bx = b.x;
+  if (ax === bx) {
+    return a.y - b.y;
+  }
+  return ax - bx;
+}
+
+function findIntersections(lines) {
+  var LEFT         = 0;
+  var INTERSECTION = 1;
+  var RIGHT        = 2;
+
+  var eventQueue = new buckets.PriorityQueue(function (a, b) {
+    return comparePoints(a.point, b.point);
+  });
+
+  lines.forEach(function (l) {
+    flipLinePoints(l);
+    eventQueue.enqueue({type: LEFT, line: l, point: l.a});
+    eventQueue.enqueue({type: RIGHT, line: l, point: l.b});
+  });
+
+  while (!eventQueue.isEmpty()) {
+    var event = eventQueue.dequeue();
+    switch (event.type) {
+      case LEFT:
+        break;
+      case INTERSECTION:
+        break;
+      case RIGHT:
+        break;
+    }
+  }
+}
